b0108ea6081a31d0fe0a654f53813da038d04904
Cleyton Albuquerque <cleyton.albuquerque@dcx.ufpb.br>
2025-05-23 14:30:28 -0300

    rota post transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,27 @@
 import { knex } from "../database.js";
 import { FastifyInstance } from "fastify";
+import { z } from 'zod'
+import crypto from 'node:crypto'
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
     
-    app.post('/', async (request) => {
-        request.body
+    app.post('/', async (request, reply) => {
+        
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit']),
+        })
+        const {title, amount, type} = createTransactionBodySchema.parse(request.body)
+
+        const transaction = await knex ('transactions').insert({
+            id: crypto.randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount -1,
+        })
+        
+        return reply.status(201).send()
 
         
     })
